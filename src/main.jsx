6ef8a908dbc160634527d685461945c5f6f810ec
Jose Mauricio Granados Muñoz <josgrana1@publicisgroupe.net>
2022-10-21 10:47:40 -0600

    Fixing the basename on subdomains

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react-router-v6-training">
       <AlbumsApp/>
     </BrowserRouter>
   </React.StrictMode>
